0ef0b1763b4174cba4db905e8b4943caf90853ad
ygm <ygm121351@163.com>
2026-05-29 09:43:41 +0800

    feat: imp useEffect prepare struct

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -5,4 +5,10 @@ export const Placement = 0b0000001;
 export const Update = 0b0000010;
 export const ChildDeletion = 0b0000100;
 
+// Fiber 节点本次更新存在副作用
+export const PassiveEffect = 0b0010000;
+
 export const MutationMask = Placement | Update | ChildDeletion;
+
+// useEffect 的依赖变化时，或函数组件卸载时，执行回调
+export const PassiveMask = PassiveEffect | ChildDeletion;
--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -11,6 +11,8 @@ import {
 import { Action } from 'shared/ReactTypes';
 import { scheduleUpdateOnFiber } from './workLoop';
 import { Lane, NoLane, requestUpdateLanes } from './fiberLanes';
+import { EffectTags, HookHasEffect, Passive } from './hookEffectTags';
+import { PassiveEffect } from './fiberFlags';
 
 let currentlyRenderingFiber: FiberNode | null = null;
 let workInProgressHook: Hook | null = null;
@@ -23,6 +25,22 @@ interface Hook {
   memoizedState: any;
   updateQueue: unknown;
   next: Hook | null;
+}
+
+export interface Effect {
+  tag: EffectTags;
+  create: EffectCallback | void;
+  destroy: EffectCallback | void;
+  deps: EffectDeps;
+  next: Effect | null;
+}
+
+type EffectCallback = () => void;
+type EffectDeps = any[] | null;
+
+// 定义函数组件的 FCUpdateQueue 数据结构
+export interface FCUpdateQueue<State> extends UpdateQueue<State> {
+  lastEffect: Effect | null;
 }
 
 export function renderWithHooks(wip: FiberNode, lane: Lane) {
@@ -54,14 +72,74 @@ export function renderWithHooks(wip: FiberNode, lane: Lane) {
 }
 
 const HooksDispatcherOnMount: Dispatcher = {
-  useState: mountState
+  useState: mountState,
+  useEffect: mountEffect
 };
 
 const HooksDispatcherOnUpdate: Dispatcher = {
-  useState: updateState
+  useState: updateState,
+  // todo-code 待实现
+  useEffect: mountEffect
 };
 
-function updateState<State>() {
+function mountEffect(create: EffectCallback | void, deps: EffectDeps | void) {
+  // 当前正在工作的 useEffect
+  const hook = mountWorkInProgressHook();
+  const nextDeps = deps == undefined ? null : deps;
+
+  (currentlyRenderingFiber as FiberNode).flags |= PassiveEffect;
+
+  hook.memoizedState = pushEffect(
+    Passive | HookHasEffect,
+    create,
+    undefined,
+    nextDeps
+  );
+}
+
+function pushEffect(
+  tag: EffectTags,
+  create: EffectCallback | void,
+  destroy: EffectCallback | void,
+  deps: EffectDeps
+): Effect {
+  const effect: Effect = {
+    tag,
+    create,
+    destroy,
+    deps,
+    next: null
+  };
+  const fiber = currentlyRenderingFiber as FiberNode;
+  const updateQueue = fiber.updateQueue as FCUpdateQueue<any>;
+  if (updateQueue === null) {
+    const newUpdateQueue = creactFCUpdateQueue();
+    fiber.updateQueue = newUpdateQueue;
+    effect.next = effect;
+    newUpdateQueue.lastEffect = effect;
+  } else {
+    // 插入 effect
+    const lastEffect = updateQueue.lastEffect;
+    if (lastEffect == null) {
+      effect.next = effect;
+      updateQueue.lastEffect = effect;
+    } else {
+      const firstEffect = lastEffect.next;
+      lastEffect.next = effect;
+      effect.next = firstEffect;
+      updateQueue.lastEffect = effect;
+    }
+  }
+  return effect;
+}
+
+function creactFCUpdateQueue<State>() {
+  const updateQueue = createUpdateQueue<State>() as FCUpdateQueue<State>;
+  updateQueue.lastEffect = null;
+  return updateQueue;
+}
+
+function updateState<State>(): [State, Dispatch<State>] {
   // 当前正在工作的 useState
   const hook = updateWorkInProgressHook();
 
@@ -107,8 +185,6 @@ function mountState<State>(
 }
 
 function updateWorkInProgressHook(): Hook {
-  // TODO render 阶段触发的更新
-
   // 保存链表中的下一个 Hook
   let nextCurrentHook: Hook | null;
   if (currentHook == null) {
